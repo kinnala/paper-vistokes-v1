9dc57aeee2341e86622586d023ed4ad39e0fa762
Tom Gustafsson <tom.gustafsson@iki.fi>
2023-08-29 10:46:17 +0300

    Add source code

---
+++ convergence2.py
@@ -0,0 +1,182 @@
+from skfem import *
+from skfem.helpers import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+hs = []
+ul2 = []
+uh1 = []
+pl2 = []
+lamhm1 = []
+
+uxfun = None
+
+for nnodes in [4, 8, 16, 32, 64]:
+    mesh = MeshQuad.init_tensor(
+        np.linspace(-1, 1, nnodes),
+        np.linspace(-1, 1, nnodes),
+    ).to_meshtri(style='x')
+
+    mesh.draw()
+    plt.savefig('figs/uniform_{}_mesh.pdf'.format(nnodes))
+
+    e = ElementVector(ElementTriP1()) * ElementTriP1() * ElementVector(ElementTriSkeletonP0())
+
+    # while not presented in the paper,
+    # it is possible to try different finite element combinations:
+
+    #e = ElementVector(ElementTriP2G()) * ElementDG(ElementTriP1()) * ElementVector(ElementTriSkeletonP1())
+    #e = ElementVector(ElementTriP2G()) * ElementTriP1() * ElementVector(ElementTriP1())
+    #e = ElementVector(ElementTriP1()) * ElementTriP1() * ElementVector(ElementTriSkeletonP1())
+    #e = ElementVector(ElementTriP1()) * ElementTriP1() * ElementVector(ElementTriP1())
+    #e = ElementVector(ElementTriP2G()) * ElementDG(ElementTriP1()) * ElementVector(ElementTriSkeletonP0())
+    #e = ElementVector(ElementTriP1()) * ElementTriP1() * ElementVector(ElementTriSkeletonP0())
+    #e = ElementVector(ElementTriP1()) * ElementTriP1() * ElementVector(ElementTriSkeletonP1())
+    #e = ElementVector(ElementTriP2()) * ElementTriP1() * ElementVector(ElementTriSkeletonP1())
+    #e = ElementVector(ElementTriP3()) * ElementTriP2() * ElementVector(ElementTriSkeletonP1())
+    #e = ElementVector(ElementTriP2()) * ElementTriP2() * ElementVector(ElementTriSkeletonP1())
+
+    basis = Basis(mesh, e)
+    fbasis = basis.boundary()
+
+    from uzawa import solver
+
+    # plotting
+    # uncomment some of the following lines to get different plots
+    x = solver(basis, fbasis)
+
+    (u, ubasis), (p, pbasis), (lam, lambasis) = basis.split(x)
+    #ubasis.plot(u, colorbar=True).show()
+    #pbasis.plot(p, colorbar=True, shading='gouraud').show()
+    #lambasis.plot(lam)
+    (lamx, lamxbasis), (lamy, lamybasis) = lambasis.split(lam)
+    #lamxbasis.plot(lamx, colorbar=True, shading='gouraud')
+    #lamybasis.plot(lamy, colorbar=True, shading='gouraud')
+    (ux, uxbasis), (uy, uybasis) = ubasis.split(u)
+    #uxbasis.plot(np.sqrt(ux ** 2 + uy ** 2), colorbar=True, nrefs=3, shading='gouraud').show()
+
+    import matplotlib.pyplot as plt
+    dlamxfun = lamxbasis.interpolator(lamx)
+    xs = np.linspace(-0.999, 0.999, 200)
+
+    import matplotlib.pyplot as plt
+    dlamyfun = lamybasis.interpolator(lamy)
+    xs = np.linspace(-0.999, 0.999, 200)
+
+    fig = plt.figure()
+    fig.set_size_inches(5/1.2, 4/1.2)
+    plt.plot(xs, dlamxfun(np.array([0*xs + 1, xs])), 'k-')
+    plt.xlabel('$y$')
+    plt.ylabel('$\lambda_n$')
+    plt.tight_layout()
+    plt.savefig('figs/uniform_{}_lambdan.pdf'.format(nnodes), dpi=50)
+
+    fig = plt.figure()
+    fig.set_size_inches(5/1.2, 4/1.2)
+    plt.plot(xs, np.abs(dlamyfun(np.array([0*xs + 1, xs]))), 'k-')
+    plt.xlabel('$y$')
+    plt.ylabel('$\|{\\bf \lambda}_t\|$')
+    plt.tight_layout()
+    plt.savefig('figs/uniform_{}_lambdat.pdf'.format(nnodes), dpi=50)
+
+    # calculate errors
+
+    if uxfun is not None:
+        @Functional
+        def ul2err(w):
+            return ((w['u'][0] - uxfun(w.x)) ** 2
+                    + (w['u'][1] - uyfun(w.x)) ** 2)
+
+        @Functional
+        def uh1err(w):
+            return ((w['u'].grad[0][0] - duxxfun(w.x)) ** 2
+                    + (w['u'].grad[0][1] - duxyfun(w.x)) ** 2
+                    + (w['u'].grad[1][0] - duyxfun(w.x)) ** 2
+                    + (w['u'].grad[1][1] - duyyfun(w.x)) ** 2)
+
+        @Functional
+        def pl2err(w):
+            return ((w['p'] - pfun(w.x)) ** 2)
+
+        @Functional
+        def lamerr(w):
+            return w.h * ((w['lam'][0] - lamxfun(w.x)) ** 2
+                          + (w['lam'][1] - lamyfun(w.x)) ** 2)
+
+        ul2.append(np.sqrt(ul2err.assemble(ubasis, u=u)))
+        uh1.append(np.sqrt(uh1err.assemble(ubasis, u=u)))
+        pl2.append(np.sqrt(pl2err.assemble(pbasis, p=p)))
+        lamhm1.append(np.sqrt(lamerr.assemble(lambasis.boundary(), lam=lam)))
+        hs.append(mesh.param())
+
+    uxfun = uxbasis.interpolator(ux)
+    uyfun = uybasis.interpolator(uy)
+
+    duxbasis = uxbasis.with_element(ElementDG(ElementTriP1()))
+    duxx = duxbasis.project(grad(uxbasis.interpolate(ux))[0])
+    duxy = duxbasis.project(grad(uxbasis.interpolate(ux))[1])
+    duyx = duxbasis.project(grad(uxbasis.interpolate(uy))[0])
+    duyy = duxbasis.project(grad(uxbasis.interpolate(uy))[1])
+
+    duxxfun = duxbasis.interpolator(duxx)
+    duxyfun = duxbasis.interpolator(duxy)
+    duyxfun = duxbasis.interpolator(duyx)
+    duyyfun = duxbasis.interpolator(duyy)
+
+    pfun = pbasis.interpolator(p)
+
+    lamxfun = lamxbasis.interpolator(lamx)
+    lamyfun = lamybasis.interpolator(lamy)
+
+
+hs = np.array(hs)
+uh1 = np.sqrt(np.array(uh1) ** 2 + np.array(ul2) ** 2)
+import matplotlib.pyplot as plt
+
+width = 5 * 1.2
+height = 4 * 1.2
+fig = plt.figure()
+fig.set_size_inches(width, height)
+plt.loglog(hs, uh1, 'ko-')
+plt.loglog(hs, hs / hs[0] * uh1[0], 'k:')
+plt.xlabel('mesh parameter')
+plt.ylabel('error')
+plt.legend(['$\|\|{\\bf u}_{2h} - {\\bf u}_h\|\|_{1,\Omega}$', '$O(h)$'])
+plt.tight_layout()
+plt.grid('on')
+plt.savefig('figs/uniform_{}_uh1.pdf'.format(nnodes), dpi=50)
+
+fig = plt.figure()
+fig.set_size_inches(width, height)
+plt.loglog(hs, pl2, 'ko-')
+plt.loglog(hs, hs / hs[0] * pl2[0], 'k:')
+plt.xlabel('mesh parameter')
+plt.ylabel('error')
+plt.legend(['$\|\|p_{2h} - p_{h}\|\|_{0,\Omega}$', '$O(h)$'])
+plt.tight_layout()
+plt.grid('on')
+plt.savefig('figs/uniform_{}_pl2.pdf'.format(nnodes), dpi=50)
+
+fig = plt.figure()
+fig.set_size_inches(width, height)
+plt.loglog(hs, lamhm1, 'ko-')
+plt.loglog(hs, hs / hs[0] * lamhm1[0], 'k:')
+plt.xlabel('mesh parameter')
+plt.ylabel('error')
+plt.legend(['$\|\|h^{1/2}({\\bf \lambda}_{2h} - {\\bf \lambda}_h)\|\|_{0,\partial \Omega}$', '$O(h)$'])
+plt.tight_layout()
+plt.grid('on')
+plt.savefig('figs/uniform_{}_lamhm1.pdf'.format(nnodes), dpi=50)
+
+total = np.sqrt(uh1 ** 2 + np.array(pl2) ** 2 + np.array(lamhm1) ** 2)
+fig = plt.figure()
+fig.set_size_inches(width, height)
+plt.loglog(hs, total, 'ko-')
+plt.loglog(hs, hs / hs[0] * total[0], 'k:')
+plt.xlabel('mesh parameter')
+plt.ylabel('error')
+plt.legend(['error', '$O(h)$'])
+plt.tight_layout()
+plt.grid('on')
+plt.savefig('figs/uniform_{}_total.pdf'.format(nnodes), dpi=50)
---
+++ uzawa.py
@@ -0,0 +1,225 @@
+from skfem import *
+from skfem.helpers import *
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import splu
+
+
+def solver(basis, fbasis, y=None):
+    alpha1 = 1e-2
+    alpha2 = 1e-2
+
+    rho = 1
+
+    # test different kappa's, e.g., space-dependent here
+    kappat_const = 0.3
+    #kappat = lambda x, y: 10 - np.isclose(x, 1) * (10 - kappat_const) * (y < 1) * (y > -1)
+    kappat = lambda x, y: x * 0 + kappat_const
+
+    N = 1e4
+    nu = 1.
+
+
+    @BilinearForm
+    def bilinf(u, p, lam, v, q, mu, w):
+        return rho * dot(u, v) + 2 * nu * ddot(sym_grad(u), sym_grad(v)) - div(v) * p - div(u) * q
+
+    @LinearForm
+    def mean(v, q, mu, w):
+        return q
+
+
+    # pressure mean value is set to zero using a Lagrange multiplier
+    m = mean.assemble(basis)
+
+    @BilinearForm
+    def stabbilinf(u, p, lam, v, q, mu, w):
+        if u.hess is not None:
+            ddu = np.array([
+                u.hess[0, 1, 1] + u.hess[1, 0, 1] + 2 * u.hess[0, 0, 0],
+                2 * u.hess[1, 1, 1] + u.hess[0, 1, 0] + u.hess[1, 0, 0],
+            ])
+        else:
+            ddu = 0.
+        if v.hess is not None:
+            ddv = np.array([
+                v.hess[0, 1, 1] + v.hess[1, 0, 1] + 2 * v.hess[0, 0, 0],
+                2 * v.hess[1, 1, 1] + v.hess[0, 0, 1] + v.hess[1, 0, 0],
+            ])
+        else:
+            ddv = 0.
+        return - alpha1 * w.h ** 2 * dot(rho * u - nu * ddu + grad(p), rho * v - nu * ddv + grad(q))
+
+    def loadf(x):
+        return np.array([-x[1], x[0]])
+
+    # plot load
+    # plotbasis = Basis(m, ElementVector(ElementTriP1()))
+    # plotbasis.plot(plotbasis.project(loadf)).show()
+
+    @LinearForm
+    def load(v, q, mu, w):
+        return dot(loadf(w.x), v)
+
+    @LinearForm
+    def stabload(v, q, mu, w):
+        f = np.array([-w.x[1], w.x[0]])
+        if v.hess is not None:
+            ddv = np.array([
+                v.hess[0, 1, 1] + v.hess[1, 0, 1] + 2 * v.hess[0, 0, 0],
+                2 * v.hess[1, 1, 1] + v.hess[0, 0, 1] + v.hess[1, 0, 0],
+            ])
+        else:
+            ddv = 0.
+        return - alpha1 * w.h ** 2 * dot(f, rho * v - nu * ddv + grad(q))
+
+    @BilinearForm
+    def mixed(u, p, lam, v, q, mu, w):
+        return - dot(lam, v) - 0 * dot(mu, u)
+
+    @BilinearForm
+    def stabmixed(u, p, lam, v, q, mu, w):
+        sigu = - p * w.n + 2 * nu * mul(sym_grad(u), w.n)
+        sigv = - q * w.n + 2 * nu * mul(sym_grad(v), w.n)
+        return - alpha2 * w.h * dot(lam - sigu, 0 * mu - sigv)
+
+
+    A = bilinf.assemble(basis)
+    f = load.assemble(basis)
+    B = mixed.assemble(fbasis)
+
+    As = stabbilinf.assemble(basis)
+    fs = stabload.assemble(basis)
+    Bs = stabmixed.assemble(fbasis)
+
+    y = basis.zeros() if y is None else y
+    (u, ubasis), (p, pbasis), (lam, lambasis) = basis.split(y)
+    (ux, uxbasis), (uy, uybasis) = ubasis.split(u)
+    (lamx, lamxbasis), (lamy, lamybasis) = lambasis.split(lam)
+
+    # projections for iteration
+    @LinearForm
+    def proj1(mux, w):
+        return w.n[0] * mux
+
+    @LinearForm
+    def proj2(mux, w):
+        return w.n[1] * mux
+
+    @BilinearForm
+    def mass(lamx, mux, w):
+        return lamx * mux
+
+    lamfbasis = lambasis.boundary(quadrature=fbasis.quadrature)
+    lamxfbasis = lamxbasis.boundary(quadrature=fbasis.quadrature)
+    ufbasis = ubasis.boundary(quadrature=fbasis.quadrature)
+    pfbasis = pbasis.boundary(quadrature=fbasis.quadrature)
+    P = proj1.assemble(lamxfbasis)
+    M = mass.assemble(lamxfbasis)
+
+    I = lamxbasis.get_dofs().all()
+    normal1 = lamxbasis.zeros()
+    normal1[I] = solve(M[I].T[I].T, P[I])
+
+
+    P = proj2.assemble(lamxfbasis)
+    M = mass.assemble(lamxfbasis)
+
+    I = lamybasis.get_dofs().all()
+    normal2 = lamybasis.zeros()
+    normal2[I] = solve(M[I].T[I].T, P[I])
+
+    residual = []
+
+    i1 = basis.get_dofs(elements=True).all(['u^1^1', 'u^2^1', 'u^2'])
+    K = A + B + As + Bs
+    F = f + fs
+    K = bmat([[K, m[:, None]],
+              [m, None]], 'csr')
+    F = np.concatenate((F, [0.]))
+    AA, bb, xx, II = condense(K, F,
+                              I=np.concatenate((i1, [len(F)-1])),
+                              x=y)
+    Alu = splu(AA)
+
+
+
+    for itr in range(int(N)):
+
+        # perform return mapping
+        lamxdofs = basis.get_dofs(elements=True).all(['u^1^3'])
+        lamydofs = basis.get_dofs(elements=True).all(['u^2^3'])
+        bnd = lamxbasis.get_dofs().all()
+        yprev = y.copy()
+        # normal tangential split
+        lam1 = y[lamxdofs][bnd]
+        lam2 = y[lamydofs][bnd]
+        lamn1 = (y[lamxdofs][bnd] * normal1[bnd]
+                 + y[lamydofs][bnd] * normal2[bnd]) * normal1[bnd]
+        lamn2 = (y[lamxdofs][bnd] * normal1[bnd]
+                 + y[lamydofs][bnd] * normal2[bnd]) * normal2[bnd]
+        lamt1 = y[lamxdofs][bnd] - lamn1
+        lamt2 = y[lamydofs][bnd] - lamn2
+
+        lami = lamfbasis.interpolate(lam)
+        pi = pfbasis.interpolate(p)
+        Pi = lami * 0
+        hf = ufbasis.default_parameters()['h']
+        nf = ufbasis.default_parameters()['n']
+        Pi[0] = pi * nf[0]
+        Pi[1] = pi * nf[1]
+        ui = ufbasis.interpolate(u)
+        Pu = lamfbasis.project(ui + alpha2 * hf * (lami - (-Pi + nu * mul(grad(ui) + transpose(grad(ui)), nf))))
+        bndx = lambasis.get_dofs().all(['u^1'])
+        bndy = lambasis.get_dofs().all(['u^2'])
+
+        # Pun1 = (Pu[bndx] * normal1[bnd] + Pu[bndy] * normal2[bnd]) * normal1[bnd]
+        # Pun2 = (Pu[bndx] * normal1[bnd] + Pu[bndy] * normal2[bnd]) * normal2[bnd]
+        # Put1 = Pu[bndx] - Pun1
+        # Put2 = Pu[bndy] - Pun2
+
+        eps = 4e-1
+        sx = lam1 - eps * Pu[bndx]
+        sy = lam2 - eps * Pu[bndy]
+
+        sxn = (sx * normal1[bnd] + sy * normal2[bnd]) * normal1[bnd]
+        syn = (sx * normal1[bnd] + sy * normal2[bnd]) * normal2[bnd]
+        sxt = sx - sxn
+        syt = sy - syn
+
+        X = lamxfbasis.project(lambda x: x[0])[bnd]
+        Y = lamxfbasis.project(lambda x: x[1])[bnd]
+
+        length = np.sqrt(sxt ** 2 + syt ** 2)
+        y = basis.zeros()
+        resx = sxn + kappat(X, Y) * sxt / np.maximum(kappat(X, Y), length)
+        resy = syn + kappat(X, Y) * syt / np.maximum(kappat(X, Y), length)
+        lamxdofs = basis.get_dofs().all(['u^1^3'])
+        lamydofs = basis.get_dofs().all(['u^2^3'])
+        y[lamxdofs] = resx
+        y[lamydofs] = resy
+
+        # solve
+        y = np.concatenate((y, [0]))
+        AA, bb, xx, II = condense(K,
+                                  F,
+                                  I=np.concatenate((i1, [len(F)-1])),
+                                  x=y)
+        y[II] = Alu.solve(bb)
+        y = y[:-1]
+
+        if itr > 2:
+            diff = np.linalg.norm(yprev - y) / np.linalg.norm(yprev)
+            residual.append(diff)
+            print("{}: {}".format(itr, diff))
+            if diff < 1e-5:
+                break
+
+        (u, ubasis), (p, pbasis), (lam, lambasis) = basis.split(y)
+        (ux, uxbasis), (uy, uybasis) = ubasis.split(u)
+        (lamx, lamxbasis), (lamy, lamybasis) = lambasis.split(lam)
+
+    #plt.figure()
+    #plt.loglog(uzawa)
+    #plt.show()
+
+    return y
